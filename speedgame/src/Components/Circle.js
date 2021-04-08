ba5e614d6aa7dc3fd465e725ea3909a11a2a2a9d
Moilanen Annika <s2100141@edu.bc.fi>
2021-04-08 15:11:24 +0300

    spoeedgame first commit

---
+++ speedgame/src/Components/Circle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Circle.css";
+
+const Circle = (props) => {
+    return (
+        <div className={`circle ${props.color}`}>
+            <p>{props.id}</p>
+        </div>
+    );
+};
+
+export default Circle;
+
